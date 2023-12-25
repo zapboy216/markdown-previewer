80c6f0677f3410141f13463e061a4e483d506e65
zapboy216 <zapboy216@netzero.com>
2023-12-25 10:28:48 -0500

    Add markdown functionality to App.js

--- src/App.js
+++ src/App.js
@@ -1,7 +1,24 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import marked from 'marked';
 
 function App() {
+  // State to hold the markdown text
+  const [markdown, setMarkdown] = useState(initialMarkdown);
+
+  // UseEffect to render the initial markdown
+  useEffect(() => {
+    const initialRender = document.getElementById('preview');
+    initialRender.innerHTML = marked(initialMarkdown);
+  }, []);
+
+  // Function to handle change in textarea and convert markdown to HTML
+  const handleChange = (event) => {
+    setMarkdown(event.target.value);
+    const renderedMarkdown = marked(event.target.value);
+    document.getElementById('preview').innerHTML = renderedMarkdown;
+  };
+
   return (
     <div className="container mt-5">
       <div className="row">
@@ -10,22 +27,44 @@ function App() {
           <p>by zapboy216</p>
         </div>
       </div>
-      <div className="row">
-        <div className="col-md-6">
-          <h2>Editor</h2>
-          <textarea id="editor" className="form-control" rows="10">
-            {/* Default markdown text will go here */}
-          </textarea>
-        </div>
-        <div className="col-md-6">
-          <h2>Preview</h2>
-          <div id="preview" className="border p-3">
-            {/* Rendered HTML from markdown will go here */}
-          </div>
-        </div>
-      </div>
+
+      
+      <textarea 
+        id="editor" 
+        className="form-control" 
+        rows="10"
+        value={markdown}
+        onChange={handleChange}
+      />
+      <div id="preview" className="border p-3"></div>
     </div>
   );
 }
+
+// Initial markdown text
+const initialMarkdown = `
+# Welcome to my React Markdown Previewer!
+
+## This is a sub-heading...
+
+[Visit GitHub](https://github.com)
+
+\`<div>some inline code</div>\`
+
+\`\`\`
+// this is a code block
+function helloWorld() {
+  console.log("Hello, world!");
+}
+\`\`\`
+
+- This is a list item
+
+> Blockquote
+
+![React Logo](https://reactjs.org/logo-og.png)
+
+**This is bold text**
+`;
 
 export default App;
