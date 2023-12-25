a7d35e10f24283fbe3f500c980a72464ae163fd4
zapboy216 <zapboy216@netzero.com>
2023-12-25 09:19:15 -0500

    Update App.js with Markdown Previewer components

--- src/App.js
+++ src/App.js
@@ -1,25 +1,31 @@
-import logo from './logo.svg';
+import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
-
-
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container mt-5">
+      <div className="row">
+        <div className="col-md-6">
+          <h2>Editor</h2>
+          <textarea id="editor" className="form-control" rows="10">
+            # Welcome to my React Markdown Previewer!
+
+## This is a sub-heading...
+
+[Visit GitHub](https://github.com)
+
+`<div>some inline code</div>`
+
+
+          </textarea>
+        </div>
+        <div className="col-md-6">
+          <h2>Preview</h2>
+          <div id="preview" className="border p-3">
+            {/* Rendered HTML from markdown will go here */}
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
