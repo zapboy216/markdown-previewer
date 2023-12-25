5d784ea24ad70ef02c3fefeaf1b170551523b519
zapboy216 <zapboy216@netzero.com>
2023-12-25 10:44:38 -0500

    Refactor markdown rendering and update UI

--- src/App.js
+++ src/App.js
@@ -1,22 +1,20 @@
 import React, { useState, useEffect } from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import marked from 'marked';
+import { marked } from 'marked'; // Import the marked library
 
 function App() {
-  // State to hold the markdown text
+  // State to store the current markdown text
   const [markdown, setMarkdown] = useState(initialMarkdown);
 
-  // UseEffect to render the initial markdown
-  useEffect(() => {
-    const initialRender = document.getElementById('preview');
-    initialRender.innerHTML = marked(initialMarkdown);
-  }, []);
-
-  // Function to handle change in textarea and convert markdown to HTML
+  // Function to handle changes in the textarea
   const handleChange = (event) => {
-    setMarkdown(event.target.value);
-    const renderedMarkdown = marked(event.target.value);
-    document.getElementById('preview').innerHTML = renderedMarkdown;
+    const newMarkdown = event.target.value;
+    setMarkdown(newMarkdown);
+  };
+
+  // Function to convert markdown text to HTML
+  const createMarkup = () => {
+    return { __html: marked(markdown) };
   };
 
   return (
@@ -24,24 +22,33 @@ function App() {
       <div className="row">
         <div className="col-12 text-center mb-4">
           <h2>Impressive Markdown Previewer</h2>
-          <p>by zapboy216</p>
         </div>
       </div>
-
-      
-      <textarea 
-        id="editor" 
-        className="form-control" 
-        rows="10"
-        value={markdown}
-        onChange={handleChange}
-      />
-      <div id="preview" className="border p-3"></div>
+      <div className="row">
+        <div className="col-md-6">
+          <h2>Editor</h2>
+          <textarea 
+            id="editor" 
+            className="form-control" 
+            rows="10"
+            value={markdown}
+            onChange={handleChange}
+          />
+        </div>
+        <div className="col-md-6">
+          <h2>Preview</h2>
+          <div 
+            id="preview" 
+            className="border p-3"
+            dangerouslySetInnerHTML={createMarkup()}
+          />
+        </div>
+      </div>
     </div>
   );
 }
 
-// Initial markdown text
+// Initial markdown to display in the editor
 const initialMarkdown = `
 # Welcome to my React Markdown Previewer!
 
@@ -62,7 +69,6 @@ function helloWorld() {
 
 > Blockquote
 
-![React Logo](https://reactjs.org/logo-og.png)
 
 **This is bold text**
 `;
